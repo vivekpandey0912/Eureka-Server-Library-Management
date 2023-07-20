05984e5423bba2af5bede3f916af82eb77413043
Vivek Pandey <vp1446392@gmail.com>
2023-07-21 01:59:57 +0530

    Enable eureka server with help of annotation of @EnableEurekaServer

---
+++ src/test/java/com/vivek/bej/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.bej.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
